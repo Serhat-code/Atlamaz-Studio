88abc6c4a5685445e44b51e9eaaa0526bc813d10
Serhat-code <Serhatcodearchitect@gmail.com>
2026-05-24 20:01:03 +0200

    correction manual chunk

--- vite.config.js
+++ vite.config.js
@@ -39,8 +39,13 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'vendor';
+            }
+            return 'deps';
+          }
         },
       },
     },
